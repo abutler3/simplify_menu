69fd142dbd4225dcd7cdc4d04fd972a14be1bab6
Andrew Butler <abutler3@gmail.com>
2019-01-23 14:05:35 -0600

    correct spelling

--- src/SimplifyMenuServiceProvider.php
+++ src/SimplifyMenuServiceProvider.php
@@ -9,7 +9,7 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
 class SimplifyMenuServiceProvider extends ServiceProviderBase implements ServiceProviderInterface {
 
 	public function alter(ContainerBuilder $container) {
-		$definition = $container->getDefiniton('menu.default_tree_manipulators');
+		$definition = $container->getDefinition('menu.default_tree_manipulators');
 		$definition->setClass('Drupal\simplify_menu\MenuLock\MenuLockTreeManipulator');
 		\Drupal::logger('Simp Menu Add')->warning('<pre><code>' . print_r($definition, TRUE) . '</code></pre>');
 
